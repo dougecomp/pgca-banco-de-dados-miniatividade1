1d7d41122430a82c41d7ca32ad7b44e451ba8585
Douglas Silva <douglaseusilva@gmail.com>
2016-08-20 01:50:00 -0300

    Conseguir fazer passar em 2 de 3 testes do Restrict. Não está retornando mais de uma linha quando deve.

--- src/operators/Restrict.java
+++ src/operators/Restrict.java
@@ -6,12 +6,84 @@ import operators.structure.Row;
 
 
 public class Restrict implements Iterable<Row>{
-
-
+    private Iterable table;
+    private String attribute;
+    private Comparison comparisonOp;
+    private Comparable value;
+    
     public Restrict(Iterable source, Comparison comparisonOp, String attribute, Comparable value) {
+        
+        this.table = source;
+        this.comparisonOp = comparisonOp;
+        this.attribute = attribute;
+        this.value = value;
+        
     }
 
     @Override
-    public Iterator<Row> iterator() {        
+    public Iterator<Row> iterator() {
+        
+        return new Iterator<Row>(){
+            private Iterator<Row> tableIterator = table.iterator();
+            
+            @Override
+            public boolean hasNext() {
+                return tableIterator.hasNext();
+            }
+
+            @Override
+            public Row next() {
+                
+                //Registro da tabela original
+                /*Row originalRow = tableIterator.next();
+
+                while(!comparisonOp.compare(originalRow.getValue(attribute), value)) {
+                    
+                    originalRow = tableIterator.next();
+                    
+                }
+                
+                //Guardando as colunas da tabela
+                String[] columns = originalRow.getAttributes();
+
+                //Registro contendo as colunas da tabela
+                Row newRow = new Row(columns);
+
+                //Copiando todos os valores de todos os campos dessa linha
+                for (int i = 0; i < columns.length; i++) {
+                    newRow.setValue(i, originalRow.getValue(columns[i]));
+                }
+                return newRow;*/
+                
+                Row newRow = null;
+                while(tableIterator.hasNext()) {
+                    
+                    //Registro da tabela original
+                    Row originalRow = tableIterator.next();
+
+                    //Copia os valores da coluna original para a nova caso o valor da coluna especificada for igual ao valor na linha atual
+                    if (comparisonOp.compare(originalRow.getValue(attribute), value)) {
+                        
+                        //Guardando as colunas da tabela
+                        String[] columns = originalRow.getAttributes();
+                        
+                        //Registro contendo as colunas da tabela
+                        newRow = new Row(columns);
+                        
+                        //Copiando todos os valores de todos os campos dessa linha
+                        for (int i = 0; i < columns.length; i++) {
+                            newRow.setValue(i, originalRow.getValue(columns[i]));
+                        }
+                        //return newRow;
+                        
+                    }
+                    
+                }
+                
+                return newRow;
+                
+            }                    
+        };
+        
     }
 }
