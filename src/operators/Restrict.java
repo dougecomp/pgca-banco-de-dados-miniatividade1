9a5845b38000160fe1b437d6a8b5f2dc0240af66
Douglas Silva <douglaseusilva@gmail.com>
2016-08-22 19:39:41 -0300

    O código está um lixo mas passou no teste do arquivo StrictTest.java

--- src/operators/Restrict.java
+++ src/operators/Restrict.java
@@ -10,6 +10,7 @@ public class Restrict implements Iterable<Row>{
     private String attribute;
     private Comparison comparisonOp;
     private Comparable value;
+    private Row nextRow;
     
     public Restrict(Iterable source, Comparison comparisonOp, String attribute, Comparable value) {
         
@@ -17,6 +18,7 @@ public class Restrict implements Iterable<Row>{
         this.comparisonOp = comparisonOp;
         this.attribute = attribute;
         this.value = value;
+        this.nextRow = null;
         
     }
 
@@ -33,29 +35,72 @@ public class Restrict implements Iterable<Row>{
 
             @Override
             public Row next() {
-                
+               
                 //Registro da tabela original
-                /*Row originalRow = tableIterator.next();
+                Row originalRow = tableIterator.next();
+                if(nextRow == null) {  
+                    
+                    while (!comparisonOp.compare(originalRow.getValue(attribute), value)) {
 
-                while(!comparisonOp.compare(originalRow.getValue(attribute), value)) {
+                        originalRow = tableIterator.next();
+
+                    }
+                    
+                    //Guardando as colunas da tabela
+                    String[] columns = originalRow.getAttributes();
+
+                    //Registro contendo as colunas da tabela
+                    Row newRow = new Row(columns);
+
+                    //Copiando todos os valores de todos os campos dessa linha
+                    for (int i = 0; i < columns.length; i++) {
+                        newRow.setValue(i, originalRow.getValue(columns[i]));
+                    }
                     
                     originalRow = tableIterator.next();
+                    while (!comparisonOp.compare(originalRow.getValue(attribute), value) && tableIterator.hasNext()) {
+
+                        originalRow = tableIterator.next();
+
+                    }
                     
+                    //Guardando as colunas da tabela
+                    columns = originalRow.getAttributes();
+
+                    //Registro contendo as colunas da tabela
+                    nextRow = new Row(columns);
+
+                    //Copiando todos os valores de todos os campos dessa linha
+                    for (int i = 0; i < columns.length; i++) {
+                        nextRow.setValue(i, originalRow.getValue(columns[i]));
+                    }
+                    
+                    return newRow;
+                    
+                } else {
+                    Row newRow = nextRow;
+                    
+                    originalRow = tableIterator.next();
+                    while (!comparisonOp.compare(originalRow.getValue(attribute), value) && tableIterator.hasNext()) {
+
+                        originalRow = tableIterator.next();
+
+                    }
+
+                    //Guardando as colunas da tabela
+                    String[] columns = originalRow.getAttributes();
+
+                    //Registro contendo as colunas da tabela
+                    nextRow = new Row(columns);
+
+                    //Copiando todos os valores de todos os campos dessa linha
+                    for (int i = 0; i < columns.length; i++) {
+                        nextRow.setValue(i, originalRow.getValue(columns[i]));
+                    }
+                    return newRow;
                 }
                 
-                //Guardando as colunas da tabela
-                String[] columns = originalRow.getAttributes();
-
-                //Registro contendo as colunas da tabela
-                Row newRow = new Row(columns);
-
-                //Copiando todos os valores de todos os campos dessa linha
-                for (int i = 0; i < columns.length; i++) {
-                    newRow.setValue(i, originalRow.getValue(columns[i]));
-                }
-                return newRow;*/
-                
-                Row newRow = null;
+                /*Row newRow = null;
                 while(tableIterator.hasNext()) {
                     
                     //Registro da tabela original
@@ -78,9 +123,7 @@ public class Restrict implements Iterable<Row>{
                         
                     }
                     
-                }
-                
-                return newRow;
+                }*/
                 
             }                    
         };
